f4228088cd58c0cb3b11ee09b3c8a732187af8eb
Rudinei Hoehn <rudineilh@gmail.com>
2020-06-03 22:56:05 -0300

    Navigation, opacity form, grids

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -5,7 +5,7 @@ function populateUFs() {
     .then((res) => res.json())
     .then((states) => {
       for (state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.sigla} - ${state.nome}</option>`;
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
       }
     });
 }
@@ -23,11 +23,14 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+  citySelect.innerHTML = `<option value="">Selecione a Cidade</option>`;
+  citySelect.disabled = true;
+
   fetch(url)
     .then((res) => res.json())
     .then((cities) => {
       for (city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
 
       citySelect.disabled = false;
@@ -37,3 +40,44 @@ function getCities(event) {
 document
   .querySelector('select[name="uf"')
   .addEventListener("change", getCities);
+
+// Itens de coleta
+// Pegar todos os li's
+const collectedItems = document.querySelector("input[name=items]");
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+for (item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  //Adicionar ou remover uma classe cm javascript
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  //Verificar se existem itens selecionados, se sim
+  //pegar os itens selecionados
+  const alreadySelected = selectedItems.findIndex((item) => {
+    const itemFound = item == itemId;
+    return itemFound;
+  });
+
+  //Se já estiver selecionado
+  if (alreadySelected >= 0) {
+    //Tirar da seleção
+    const filteredItems = selectedItems.filter((item) => {
+      const itemsIsDifferent = item != itemId;
+      return itemsIsDifferent;
+    });
+
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId);
+  }
+
+  collectedItems.value = selectedItems;
+}
